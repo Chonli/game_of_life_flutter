dba7665fdc2f1bfe4e3b33915c074ba38724a46b
Chonli <Chonli@users.noreply.github.com>
2019-10-30 15:05:11 +0100

    Fix problems and add License

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Game of life',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Game of life'),
@@ -29,15 +19,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -51,8 +32,8 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _isRunning = false;
   Timer _loopGame;
   List<List<int>> _matrixGrid;
-  int _matrixRowSize = 20;
-  int _matrixColumnSize = 20;
+  int _matrixRowSize = 25;
+  int _matrixColumnSize = 25;
 
   @override
   void initState() {
@@ -61,15 +42,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void initMatrix() {
-    _matrixGrid = List<List<int>>(_matrixColumnSize);
-    for (var i = 0; i < _matrixColumnSize; i++) {
-      List<int> row = List<int>(_matrixRowSize);
-      for (var j = 0; j < _matrixRowSize; j++) {
-        row[j] = 0;
-      }
-
-      _matrixGrid[i] = row;
-    }
+    _matrixGrid = List.generate(
+        _matrixColumnSize, (_) => List.generate(_matrixRowSize, (_) => 0));
   }
 
   void _startStopGame() {
@@ -85,46 +59,54 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  int _testCells(int x, int y) {
-    var _nbCellNoEmpty = 0;
-    //Test cells around
-    for (var i = -1; i <= 1; i++) {
-      for (var j = -1; j < 1; j++) {
-        var tmpX = x + i, tmpY = y + j;
-        //no test current cells and out of bound cells
-        if (i == 0 && j == 0 ||
-            tmpX < 0 ||
-            tmpY < 0 ||
-            tmpX > _matrixColumnSize ||
-            tmpY > _matrixRowSize) {
-          continue;
+  void _runGame(Timer timer) {
+    final tmpmatrixGrid = List.generate(
+        _matrixColumnSize, (_) => List.generate(_matrixRowSize, (_) => 0));
+    var tmpCouterCells = 0;
+    for (var x = 0; x < _matrixColumnSize; x++) {
+      for (var y = 0; y < _matrixRowSize; y++) {
+        var nbCellNoEmpty = 0;
+        //Test cells around
+        for (var i = -1; i <= 1; i++) {
+          for (var j = -1; j <= 1; j++) {
+            var tmpX = x + i, tmpY = y + j;
+            //no test current cells and out of bound cells
+            if ((i == 0 && j == 0) ||
+                tmpX < 0 ||
+                tmpY < 0 ||
+                tmpX >= _matrixColumnSize ||
+                tmpY >= _matrixRowSize) {
+              continue;
+            }
+
+            if (_matrixGrid[tmpX][tmpY] == 1) {
+              nbCellNoEmpty++;
+            }
+          }
         }
 
-        if (_matrixGrid[tmpX][tmpY] == 1) _nbCellNoEmpty++;
+        if (_matrixGrid[x][y] == 0 && nbCellNoEmpty == 3) {
+          tmpmatrixGrid[x][y] = 1;
+          tmpCouterCells++;
+        } else if (_matrixGrid[x][y] == 1 &&
+            (nbCellNoEmpty > 3 || nbCellNoEmpty < 2)) {
+          tmpmatrixGrid[x][y] = 0;
+        } else {
+          tmpmatrixGrid[x][y] = _matrixGrid[x][y];
+          if (_matrixGrid[x][y] == 1) {
+            tmpCouterCells++;
+          }
+        }
+
+        print(
+            "[$x,$y] ${_matrixGrid[x][y]} ==[$nbCellNoEmpty]=> ${tmpmatrixGrid[x][y]}");
       }
     }
-
-    if (_matrixGrid[x][y] == 0 && _nbCellNoEmpty == 3) {
-      _matrixGrid[x][y] = 1;
-    } else if (_matrixGrid[x][y] == 1 &&
-        (_nbCellNoEmpty != 3 || _nbCellNoEmpty != 2)) {
-      _matrixGrid[x][y] = 0;
-    }
-    return _matrixGrid[x][y];
-  }
-
-  void _runGame(Timer timer) {
-    var tmpCouterCells = 0;
-    for (var i = 0; i < _matrixColumnSize; i++) {
-      for (var j = 0; j < _matrixRowSize; j++) {
-        tmpCouterCells += _testCells(i, j);
-      }
-
-      setState(() {
-        _counterLoop++;
-        _counterCells = tmpCouterCells;
-      });
-    }
+    setState(() {
+      _counterLoop++;
+      _counterCells = tmpCouterCells;
+      _matrixGrid = tmpmatrixGrid;
+    });
   }
 
   @override
@@ -174,9 +156,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildGridItems(BuildContext context, int index) {
-    int gridStateLength = _matrixRowSize;
-    final x = (index / gridStateLength).floor();
-    final y = (index % gridStateLength);
+    final x = (index / _matrixRowSize).floor();
+    final y = (index % _matrixColumnSize);
     return GestureDetector(
       onTap: !_isRunning
           ? () {
